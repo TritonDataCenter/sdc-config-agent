114c1dca86271e27fe3a7332d5592bd1a3693006
Andres Rodriguez <andres@joyent.com>
2014-08-25 17:19:39 -0700

    TOOLS-607 I for one, welcome my new open source license

--- agent.js
+++ agent.js
@@ -1,6 +1,14 @@
 /*
- * Copyright (c) 2013, Joyent, Inc. All rights reserved.
- *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+/*
  * agent.js: SDC configuration agent
  *
  * This agent queries SAPI for changes to a zone's configuration, downloads
